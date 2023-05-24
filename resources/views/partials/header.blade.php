2a07f09e7d7e31e7cde94e20a89f002901785362
simolorevg <simoneviaggi@libero.it>
2023-05-24 16:38:31 +0200

    project done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar bg-light">
     <div class="container-fluid d-flex justify-content-center align-items-center">
         <a class="navbar-brand" href="#">
-            <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            <img src="{{ Vite::asset('resources\img\logo-no-background.svg') }}" alt="Logo" width="100"
+                height="100" class="d-inline-block align-text-top">
         </a>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container-fluid">
-        <div class="row row-cols-3">
+    <div class="container">
+        <div class="row row-cols-3 d-flex justify-content-center align-items-center">
             @foreach ($movies as $item)
                 <div class="col">
-
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $item->image }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5>{{ $item->title }}</h5>
+                            <p>Titolo originale: {{ $item->original_title }}</p>
+                            <p>Uscito il: {{ $item->date }}</p>
+                            <p>Voto: {{ $item->vote }}</p>
+                        </div>
+                    </div>
                 </div>
             @endforeach
         </div>
